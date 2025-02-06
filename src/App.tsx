9d13606556b19a9b1f2235e377923fbfc9191ac1
infinitelife <infinitelife102@gmail.com>
2025-02-06 10:19:21 +0800

    commit: deploy githubpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from '@/components/ui/sonner';
 import { AuthProvider } from '@/contexts/AuthContext';
